80ff9cfccaaa1b5cc9999939d7e4e119aac9dae4
Kania <kania.praca@wp.pl>
2024-06-20 20:54:01 +0200

    Create Book and feedback entities

---
+++ src/main/java/com/christianj98/book/book/Book.java
@@ -0,0 +1,26 @@
+package com.christianj98.book.book;
+
+import com.christianj98.book.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+}
---
+++ src/main/java/com/christianj98/book/feedback/Feedback.java
@@ -0,0 +1,21 @@
+package com.christianj98.book.feedback;
+
+import com.christianj98.book.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Double note;
+    private String comment;
+}
